3d753ab30cec359d948c61e102650a830a43c9d9
Lucky-Nicke <glucky.nicke@gmail.com>
2025-06-10 09:00:50 +0800

    创建项目
    图书馆借阅系统

---
+++ src/main/java/club/lanxige/bbmm/BbmmApplication.java
@@ -0,0 +1,13 @@
+package club.lanxige.bbmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/lanxige/bbmm/BbmmApplicationTests.java
@@ -0,0 +1,13 @@
+package club.lanxige.bbmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
